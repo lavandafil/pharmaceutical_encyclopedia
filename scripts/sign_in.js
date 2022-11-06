773186aab041b6d3ff2ac11731ce93e1998bd6d0
reincarndeity@3cloud.space <100484548+paranoiiiic@users.noreply.github.com>
2022-11-06 21:26:20 +0300

    additional js scripts to sign in form

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -1,5 +1,7 @@
 let auth_cont = document.getElementById('auth_container');
 let reg_cont = document.getElementById('reg_container');
+let pass_t = document.getElementById('pass_trouble');
+let form_p = document.getElementById('form_prob');
 
 function regShow(){
     auth_cont.style.display = 'none';
@@ -8,4 +10,15 @@ function regShow(){
 function goBack(){
     auth_cont.style.display = 'block';
     reg_cont.style.display = 'none';
+}
+function toBack(){
+    pass_t.style.display = 'none';
+    auth_cont.style.display = 'block';
+}
+function forgetPass(){
+    pass_t.style.display = 'block';
+    auth_cont.style.display = 'none';
+}
+function continueReset(){
+    form_p.style.display = 'none';
 }
