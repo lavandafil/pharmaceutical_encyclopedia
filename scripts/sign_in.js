bae26716b104ab98bf78a927f2c47d79bed7c67e
reincarndeity@3cloud.space <100484548+paranoiiiic@users.noreply.github.com>
2022-11-06 23:24:52 +0300

    fixes

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -2,6 +2,8 @@ let auth_cont = document.getElementById('auth_container');
 let reg_cont = document.getElementById('reg_container');
 let pass_t = document.getElementById('pass_trouble');
 let form_p = document.getElementById('form_prob');
+let form_res = document.getElementById('form_reset');
+let last_et = document.getElementById('last_et');
 
 function regShow(){
     auth_cont.style.display = 'none';
@@ -21,4 +23,10 @@ function forgetPass(){
 }
 function continueReset(){
     form_p.style.display = 'none';
+    form_res.style.display = 'block';
+}
+function showLast(){
+    pass_t.style.height = '290px';
+    form_res.style.display = 'none';
+    last_et.style.display = 'block';
 }
