a04cb1108ef98667e800172e35c604b7afea2e36
lavandafil <117371129+lavandafil@users.noreply.github.com>
2022-11-11 21:07:04 +0300

    sign_in

---
+++ pgs/pharmaceutical_encyclopedia-main/scripts/sign_in.js
@@ -0,0 +1,43 @@
+let auth_cont = document.getElementById('auth_container');
+let reg_cont = document.getElementById('reg_container');
+let pass_t = document.getElementById('pass_trouble');
+let form_p = document.getElementById('form_prob');
+let form_res = document.getElementById('form_reset');
+let last_et = document.getElementById('last_et');
+let showp = document.getElementById('show_pass');
+let pass_f = document.getElementById('pass_field');
+function regShow(){
+    auth_cont.style.display = 'none';
+    reg_cont.style.display = 'block';
+}
+function goBack(){
+    auth_cont.style.display = 'block';
+    reg_cont.style.display = 'none';
+}
+function toBack(){
+    pass_t.style.display = 'none';
+    auth_cont.style.display = 'block';
+}
+function forgetPass(){
+    pass_t.style.display = 'block';
+    auth_cont.style.display = 'none';
+}
+function continueReset(){
+    form_p.style.display = 'none';
+    form_res.style.display = 'block';
+}
+function showLast(){
+    pass_t.style.height = '290px';
+    form_res.style.display = 'none';
+    last_et.style.display = 'block';
+}
+function showPass(){
+    if(showp.className == 'fa-regular fa-eye eye'){
+        showp.className = 'fa-regular fa-eye-slash eye';
+        pass_f.type = 'text';
+    }
+    else{
+        showp.className = 'fa-regular fa-eye eye';
+        pass_f.type = 'password';
+    }
+}
