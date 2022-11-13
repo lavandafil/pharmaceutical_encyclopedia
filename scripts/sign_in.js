b55d9c9a97230ef25f1d936d00a5a3dc3f18fa81
lavandafil <117371129+lavandafil@users.noreply.github.com>
2022-11-13 17:46:17 +0300

    sign_in

--- scripts/sign_in.js
+++ scripts/sign_in.js
@@ -4,6 +4,11 @@ let pass_t = document.getElementById('pass_trouble');
 let form_p = document.getElementById('form_prob');
 let form_res = document.getElementById('form_reset');
 let last_et = document.getElementById('last_et');
+let name = document.getElementById('name_field');
+let pass_first = document.getElementById('pass_first');
+let pass_second = document.getElementById('pass_second');
+let showpass = document.getElementById('show_pass');
+let pass_field = document.getElementById('pass_field');
 
 function regShow(){
     auth_cont.style.display = 'none';
@@ -29,4 +34,23 @@ function showLast(){
     pass_t.style.height = '290px';
     form_res.style.display = 'none';
     last_et.style.display = 'block';
+}
+function showPass(){
+    if(showpass.className === 'fa-regular fa-eye eye'){
+        showpass.className = 'fa-regular fa-eye-slash eye'
+        pass_field.type = 'text';
+    }
+    else{
+        showpass.className = 'fa-regular fa-eye eye';
+        pass_field.type = 'password';
+    }
+}
+
+function checkValid(){
+let regex = RegExp('^[а-яА-ЯёЁ0-9\\s]+$ ');
+}
+function check(){
+    let intput = name.value.replace(/[A-Za-z0-9]+/g, '');
+    console.log(intput);
+    name.value = intput;
 }
